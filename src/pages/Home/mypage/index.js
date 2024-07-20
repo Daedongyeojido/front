1b68dc8eb74e6e3b795e1ab37f6f5bb066678bd5
daeunleeeee <lde7953@gmail.com>
2024-07-21 04:29:43 +0900

    Revert "✨ [ADD] AppBar 컴포넌트 추가"
    This reverts commit f6006b235ea44b2bff8e505eab81e8051486915d.

--- src/pages/Home/mypage/index.js
+++ src/pages/Home/mypage/index.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import { PageContainer, ContentContainer } from '../../../components/Layout';
-import AppBar from '../../../components/AppBar';
+import { PageContainer, ContentContainer } from '../../../components/Layout'
+
 
 function Mypage() {
+
     return (
+    <>
         <PageContainer>
-        <AppBar title="마이페이지" />
-        <ContentContainer>
-            <div>
-            여긴 마이페이지지
-            </div>
-        </ContentContainer>
+            <ContentContainer>
+                <div>
+                    여긴 마이페이지지
+                </div>
+            </ContentContainer>
         </PageContainer>
+    </>
     );
-}
+    }
 
 export default Mypage;
+
--- src/pages/Home/tutorial/index.js
+++ src/pages/Home/tutorial/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { PageContainer, ContentContainer } from '../../../components/Layout'
-import AppBar from '../../../components/AppBar';
 
 
 function Tutorial() {
+
     return (
+    <>
         <PageContainer>
-        <AppBar title="튜토리얼" />
             <ContentContainer>
                 <div>
                     여긴 튜토리얼이지
                 </div>
             </ContentContainer>
         </PageContainer>
+    </>
     );
     }
 
