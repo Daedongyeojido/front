0ee92da836d7531a40e6d61fead74e4144696246
daeunleeeee <lde7953@gmail.com>
2024-07-26 01:28:23 +0900

    ✨ [FEAT] 홈 - 카카오맵api 연동
    출발, 도착 입력값 모달창에서 받고 선택

---
+++ src/pages/Map/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import styled from "styled-components";
+import { PageContainer, ContentContainer } from '../../components/Layout'
+
+
+
+function Map() {
+    // const navigate = useNavigate();
+    return (
+        <>
+        <PageContainer>
+            <ContentContainer>
+
+            </ContentContainer>
+        </PageContainer>
+        </>
+    );
+}
+
+export default Map;
