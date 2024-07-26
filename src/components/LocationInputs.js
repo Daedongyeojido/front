88eeeeea6e4dfba52ff79d8893336f07b2fbf708
daeunleeeee <lde7953@gmail.com>
2024-07-26 16:29:48 +0900

    ⚡️ [ADD] isInputComplete 기능 추가
    출발, 도착 아이콘 누락 수정

--- src/components/LocationInputs.js
+++ src/components/LocationInputs.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import styled from 'styled-components';
 import Button from "./Button";
+import Departure from '../Image/DepartureImg.png';
+import Arrival from '../Image/ArrivalImg.png';
 
 const Container = styled.div`
   width: 100%;
@@ -16,54 +18,76 @@ const Container = styled.div`
   z-index: 2;
 `;
 
-const InputBox = styled.input`
+const InputContainer = styled.div`
   width: 85%;
   height: 50px;
-  font-size: 15px;
-  flex-shrink: 0;
-  justify-content: center;
-  align-items: center;
   background-color: #eeeeee;
   border-radius: 10px;
   box-shadow: 0px 5px 5px rgba(0, 0, 0, 0.25);
-  border: none;
-  padding: 0 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 15px;
   margin: 10px 0;
+`;
+
+const Icon = styled.img`
+  margin-right: 10px;
+  width: 20px;
+  height: 20px;
+`;
+
+const InputBox = styled.input`
+  width: 100%;
+  height: 100%;
+  font-size: 15px;
+  background-color: transparent;
+  border: none;
   outline: none;
   &:focus {
-    outline: none;
     border-color: #789ddd;
   }
 `;
 
+const LocationInputs = ({ startPoint, endPoint, handleInputClick, handleGo }) => {
+  const isInputComplete = startPoint.name && endPoint.name;
 
-const LocationInputs = ({ startPoint, endPoint, handleInputClick, handleGo }) => (
-
-  <Container>
-    <InputBox
-      placeholder="출발지를 입력해주세요"
-      value={startPoint.name}
-      onClick={() => handleInputClick(true)}
-      readOnly
-    />
-    <InputBox
-      placeholder="도착지를 입력해주세요"
-      value={endPoint.name}
-      onClick={() => handleInputClick(false)}
-      readOnly
-    />
-    <InputBox placeholder="추천받고 싶은 않은 장소가 있어요!" />
-    <Button
-      fontSize="20px"
-      width="60%"
-      height="49px"
-      borderradius="30px"
-      backgroundColor="#789DDD"
-      onClick={handleGo}
-    >
-      GO
-    </Button>
-  </Container>
-);
+  return (
+    <Container>
+      <InputContainer>
+        <Icon src={Departure} alt="출발" />
+        <InputBox
+          placeholder="출발지를 입력해주세요"
+          value={startPoint.name}
+          onClick={() => handleInputClick(true)}
+          readOnly
+        />
+      </InputContainer>
+      <InputContainer>
+        <Icon src={Arrival} alt="도착" />
+        <InputBox
+          placeholder="도착지를 입력해주세요"
+          value={endPoint.name}
+          onClick={() => handleInputClick(false)}
+          readOnly
+        />
+      </InputContainer>
+      <InputContainer>
+        <InputBox placeholder="추천받고 싶은 않은 장소가 있어요!" />
+      </InputContainer>
+      <Button
+        fontSize="20px"
+        width="60%"
+        height="49px"
+        borderradius="30px"
+        backgroundColor={isInputComplete ? "#789DDD" : "#CCCCCC"}
+        onClick={handleGo}
+        disabled={!isInputComplete}
+      >
+        GO
+      </Button>
+    </Container>
+  );
+};
 
 export default LocationInputs;
