fb8894c9adde05f51ea8c15f7b73be0b5cc299fd
daeunleeeee <lde7953@gmail.com>
2024-07-28 02:15:57 +0900

    ✨ [FEAT] splash screen 추가

---
+++ src/pages/splash.js
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { PageContainer, ContentContainer } from '../components/Layout';
+import styled, { keyframes } from 'styled-components';
+import logo from './Auth/signin/logo.png';
+
+const fadeInOut = keyframes`
+    0% { opacity: 0; }
+    50% { opacity: 1; }
+`;
+
+const LogoContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    margin-top: 150px;
+    margin-bottom: 70px;
+    animation: ${fadeInOut} 3s ease-in-out;
+    animation-fill-mode: forwards;
+`;
+
+const Text = styled.p`
+    color: ${({theme}) => theme.darkgrayColor};
+    font-size: 25px;
+    text-align: center;
+    animation: ${fadeInOut} 3s ease-in-out;
+    animation-fill-mode: forwards;
+`;
+
+const SplashWrapper = styled.div`
+    display: {isHidden ? 'none' : 'block'};
+`;
+
+const SplashScreen = () => {
+    const navigate = useNavigate();
+    const [isHidden, setIsHidden] = useState(false);
+
+  //TODO: 인증된 사용자면 홈 화면으로 이동, 그렇지 않으면 로그인 페이징로 이동
+  //로그인, 회원가입 api 붙일 때 로직 점검하기
+
+    useEffect(() => {
+        const checkAuth = () => {
+        const isAuthenticated = false;
+        return isAuthenticated;
+        };
+
+        const timer = setTimeout(() => {
+        setIsHidden(true);
+        if (checkAuth()) {
+            navigate('/main');
+        } else {
+            navigate('/signin');
+        }
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    }, [navigate]);
+
+    return (
+        <SplashWrapper isHidden={isHidden}>
+        <PageContainer style={{ backgroundColor: 'white' }}>
+            <ContentContainer>
+            <LogoContainer>
+                <img src={logo} width='200px' alt="로고" />
+            </LogoContainer>
+            <Text>
+                일상 속 건강한 여정을 떠나요
+            </Text>
+            </ContentContainer>
+        </PageContainer>
+        </SplashWrapper>
+    );
+    };
+
+export default SplashScreen;
--- src/utils/route/Routes.js
+++ src/utils/route/Routes.js
@@ -1,39 +1,21 @@
-// import React from 'react';
-// import { Route, Routes } from 'react-router-dom';
-// import Main from './pages/Home/main';
-// import Tutorial from './pages/Tutorial';
-// import Mypage from './pages/Mypage';
-
-// const App = () => {
-//   return (
-//     <Routes>
-//       <Route path='/' element={<Main />} >
-//         <Route path='/tutorial' element={<Tutorial />} />
-//         <Route path='/mypage' element={<Mypage />} />
-//       </Route>
-//     </Routes>
-//   );
-// }
-// export default App;
-
-
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Main from '../../pages/Home/main';
 import Tutorial from '../../pages/Home/tutorial/tutorial';
 import Mypage from '../../pages/Home/mypage/index';
 import Signin from '../../pages/Auth/signin/index';
 import Signup from '../../pages/Auth/signup/index';
 import Map from '../../pages/Map';
+import Home from '../../pages/Home/main';
+import SplashScreen from '../../pages/splash';
 
 export const routes = [
-    { path: '/', element: <Main /> },
+    { path: '/', element: <SplashScreen /> },
+    { path: '/main', element: <Home /> },
     { path: '/tutorial', element: <Tutorial /> },
     { path: '/mypage', element: <Mypage /> },
     { path: '/signup', element: <Signup /> },
     { path: '/signin', element: <Signin /> },
     { path: '/map', element: <Map /> }
-
 ];
 
 const RoutesSetting = () => (
