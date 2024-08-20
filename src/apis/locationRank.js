2ddcd8d17970ed6864f9c453dda9c56022948e7c
조윤해 <choyunhae@joyunhaeui-MacBookPro.local>
2024-08-20 16:24:16 +0900

    회원가입, 로그인 에러 해결

---
+++ src/apis/locationRank.js
@@ -0,0 +1,20 @@
+import instance from "./instance";
+
+//추천 순위 api
+export const locationRank = async () => {
+    try {
+      const response = await instance.get('/top_recommended_places/');
+      return response.data;
+    } catch (error) {
+      console.error('추천 순위 불러오기 error:', error); 
+
+      if (error.response && error.response.data) {
+        const data = error.response.data;
+        console.log('Error response data:', error);
+      }
+      throw { general: '오류가 발생했습니다.' };
+    }
+    
+  };
+
+  
--- src/apis/myRoutesApi.js
+++ src/apis/myRoutesApi.js
@@ -2,17 +2,17 @@ import instance from "./instance";
 import {useEffect, useState} from 'react';
 
 //내 경로 모아보기 
-// export const myRoutes = async () => {
-//     try{
-//         const response = await instance.get('/user/routes' )
-//         return response.data;
-//     } catch(error) {
-//         if (error.response && error.response.data) {
-//             throw error.response.data;
-//           }
-//           throw error;
-//     }
-// }
+export const myRoutes = async () => {
+    try{
+        const response = await instance.get('/route/routes/')
+        return response.data;
+    } catch(error) {
+        if (error.response && error.response.data) {
+            throw error.response.data;
+          }
+          throw error;
+    }
+}
 
 function MyRoutesApi() {
     const [routes, setRoutes] = useState([]);
